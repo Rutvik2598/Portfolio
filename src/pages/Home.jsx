049f8ba4309eeeb7a64d8d7328f9f07765c46531
Rutvik Vishwas Kulkarni <kulkarnirutvikvishwas@gmail.com>
2025-09-23 20:49:46 -0400

    Added hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "@/components/ThemeToggle"
 import {StarBackground} from "@/components/StarBackground"
 import { Navbar } from "@/components/Navbar"
+import { HeroSection } from "@/components/HeroSection"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-forground overflow-x-hidden">
@@ -14,6 +15,10 @@ export const Home = () => {
         <Navbar/>
 
         { /* Main content */ }
+
+        <main>
+            <HeroSection/>
+        </main>
 
 
         { /* Footer */ }
